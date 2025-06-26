c1ed321ae7dc59d74cb634432d87db09e92e4ea0
deepshika196 <131590299+deepshika196@users.noreply.github.com>
2025-06-26 22:49:37 +0530

    changes for the homepage made

--- homify-website/src/components/Herosection.jsx
+++ homify-website/src/components/Herosection.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../styles/HeroSection.css';
-import heroImage from '../assets/Electrician.jpg';
-import serviceImage from '../assets/Ac_Mechanic.jpg'; // Replace with actual image
-
+import heroImage from '../assets/Electrician.png';
+import serviceImage from '../assets/Ac_Mechanic.jpg'; 
+import bgImage from '../assets/background.png';
+import community from '../assets/community.png';
+import complaint from '../assets/complaints.png';
+import user from '../assets/users.png';
+import pay from '../assets/pay.png';
 function HeroSection() {
     return (
         <>
-            <header className="hero-section">
+            <header className="hero-section " style={{
+                    backgroundImage: `url(${bgImage})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}>
                 <div className="hero-left">
                     <h1>Flatify</h1>
                     <h2>Your Flat, Our Priority</h2>
@@ -19,6 +27,28 @@ function HeroSection() {
                     <img src={heroImage} alt="Hero" />
                 </div>
             </header>
+            <section className="usermanage-section">
+                <div className="usermanage-left">
+                    <img src={user} alt="Service" />
+                </div>
+                <div className="usermanage-right">
+                    <h2>User Access & Management</h2>
+                    <p>
+                       Flatify supports multiple user logins, tailored to the needs of each role within the community. Whether you're a resident, worker, or administrator, each user gets a personalized dashboard to perform their respective tasks securely and efficiently.
+                    </p>
+                </div>
+            </section>
+            <section className='payment-section'>
+                <div className="payment-left">
+                    <h2>Simplified Rent Collection</h2>
+                    <p>
+                       For administrators, Flatify offers a streamlined rent monitoring system. Rent collection is made easier and more transparent for residents through a secure online payment portal, ensuring timely payments and hassle-free tracking.
+                    </p>
+                </div>
+                <div className="payment-right">
+                    <img src={pay} alt="Community" />
+                </div>
+            </section>
 
             <section className="maintenance-section">
                 <div className="maintenance-left">
@@ -40,13 +70,13 @@ function HeroSection() {
                     </p>
                 </div>
                 <div className="community-right">
-                    <img src={serviceImage} alt="Community" />
+                    <img src={community} alt="Community" />
                 </div>
             </section>
 
             <section className="complaint-section">
                 <div className="complaint-left">
-                    <img src={serviceImage} alt="Complaint" />
+                    <img src={complaint} alt="Complaint" />
                 </div>
                 <div className="complaint-right">
                     <h2>Complaint Registration</h2>
